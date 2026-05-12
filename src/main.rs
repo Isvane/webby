5770187bc16ed66380f0c75d4193444f399d7af2
Isvane <277444536+Isvane@users.noreply.github.com>
2026-05-13 00:09:02 +0700

    feat: implement ApiResponse enum and list_users routes

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use axum::{
     extract::Path,
     extract::Query,
     http::StatusCode,
+    response::{IntoResponse, Response},
     routing::{get, post},
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 struct Pagination {
@@ -13,16 +14,31 @@ struct Pagination {
     per_page: Option<u32>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct CreateUser {
     name: String,
     email: String,
+}
+
+enum ApiResponse {
+    Json(Vec<CreateUser>),
+    Message(StatusCode, String),
+}
+
+impl IntoResponse for ApiResponse {
+    fn into_response(self) -> Response {
+        match self {
+            Self::Json(data) => (StatusCode::OK, Json(data)).into_response(),
+            Self::Message(status, msg) => (status, msg).into_response(),
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let user_routes = Router::new()
         .route("/", get(about))
+        .route("/list", get(list_users))
         .route("/create", post(create_user))
         .route("/greet/{name}", get(greet_user));
 
@@ -42,12 +58,12 @@ async fn index() -> (StatusCode, &'static str) {
 async fn about() -> &'static str {
     "About"
 }
-async fn greet_user(Path(name): Path<String>) -> (StatusCode, String) {
-    (StatusCode::OK, format!("Hello, {name}"))
+async fn greet_user(Path(name): Path<String>) -> ApiResponse {
+    ApiResponse::Message(StatusCode::OK, format!("Hello {name}"))
 }
 
-async fn create_user(Json(input): Json<CreateUser>) -> (StatusCode, String) {
-    (
+async fn create_user(Json(input): Json<CreateUser>) -> ApiResponse {
+    ApiResponse::Message(
         StatusCode::CREATED,
         format!("Created user: {} ({})", input.name, input.email),
     )
@@ -57,4 +73,11 @@ async fn list_items(Query(pagination): Query<Pagination>) -> String {
     let page = pagination.page.unwrap_or(1);
     let per_page = pagination.per_page.unwrap_or(20);
     format!("Page {page}, {per_page} items")
+}
+
+async fn list_users() -> ApiResponse {
+    ApiResponse::Json(vec![CreateUser {
+        email: "alice@mail.com".into(),
+        name: "Alice".into(),
+    }])
 }
